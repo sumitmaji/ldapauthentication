a919c42d5fe1526f1fda883abe7045e9b1383a9f
sumit <skmaji1@outlook.com>
2022-09-12 20:30:15 +0530

    Initial commit

--- src/main/java/com/sum/security/IngressMemWebSecurityConfig.java
+++ src/main/java/com/sum/security/IngressMemWebSecurityConfig.java
@@ -1,14 +1,9 @@
 package com.sum.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.ldap.core.support.BaseLdapPathContextSource;
-import org.springframework.ldap.core.support.LdapContextSource;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,8 +14,8 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
-@Profile(value = "default")
-public class IngressLdapWebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Profile(value = "local")
+public class IngressMemWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     private AuthenticationSuccessHandler getSuccessHandler() {
@@ -63,68 +58,20 @@ public class IngressLdapWebSecurityConfig extends WebSecurityConfigurerAdapter {
         ;
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .ldapAuthentication()
-                .userSearchFilter("(cn={0})") // username which would be search in ldap. cn contains username
-                .contextSource(contextSource());
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
-    @Value(value = "${ldap.url}")
-    private String url;
-
-    @Value(value = "${ldap.basedn}")
-    private String baseDn;
-
-    @Value(value = "${ldap.admindn}")
-    private String adminDn;
-
-    @Value(value = "${ldap.password}")
-    private String password;
-
-    /**
-     * Ldap Configuration
-     *
-     * @return
-     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("sumit")
+                .password(passwordEncoder().encode("sumit")).roles("USER");
+    }
 
     @Bean
-    public BaseLdapPathContextSource contextSource() {
-        LdapContextSource bean = new LdapContextSource();
-        bean.setUrl("ldap://ldap.default.svc.cloud.uat"); // Url where ldap application is running
-        bean.setBase("dc=default,dc=svc,dc=cloud,dc=uat"); // Base DN
-        bean.setUserDn("cn=admin,dc=default,dc=svc,dc=cloud,dc=uat"); // Admin User DN
-        bean.setPassword("sumit");
-        bean.setPooled(true);
-        bean.setAnonymousReadOnly(false);
-        bean.afterPropertiesSet();
-        return bean;
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
     }
-
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-//    //Local authentication testing
-//
-//    @Bean
-//    PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("sumit")
-//                .password(passwordEncoder().encode("sumit")).roles("USER");
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsServiceBean() throws Exception {
-//        return super.userDetailsServiceBean();
-//    }
 }
